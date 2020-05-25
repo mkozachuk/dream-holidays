e5867b76e667f683a63e12d67983e2f2ec3be3c6
Maksym <maksym.kozachuk@protonmail.com>
2020-05-25 12:49:26 +0200

    initial

---
+++ src/main/java/pl/devodds/mkozachuk/springdh/SpringdhApplication.java
@@ -0,0 +1,13 @@
+package pl.devodds.mkozachuk.springdh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdhApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringdhApplication.class, args);
+    }
+
+}
---
+++ src/main/java/pl/devodds/mkozachuk/springdh/models/User.java
@@ -0,0 +1,8 @@
+package pl.devodds.mkozachuk.springdh.models;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+}
